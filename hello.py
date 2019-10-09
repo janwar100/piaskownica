2a86538290bc153d0d437a47b7952e6bea1430e6
Roman Putanowicz <putanowr@IN.THE.ANTHILL>
2019-10-09 12:20:46 +0200

    Added new file for Python

---
+++ hello.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print('Hello World')
+name = 'Roman'
+print(f"My name is {name}")
+a = 12
+b = 5
+print(f" To : {a} jest większe niż to: {b}")
